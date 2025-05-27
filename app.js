34e70b500a61e20c5a2a67ddd08e1274bd7b60f6
Mylena Ribeiro <ribeiro.mylena1@gmail.com>
2025-05-27 13:15:51 -0300

    projeto sorteador de números aleatórios

---
+++ app.js
@@ -0,0 +1,54 @@
+function sortear() {
+    let quantidade = parseInt(document.getElementById('quantidade').value);
+    let de = parseInt(document.getElementById('de').value);
+    let ate = parseInt(document.getElementById('ate').value);
+
+    // alert(`Quantidade: ${quantidade}`);
+    // alert(`Do número: ${de}`);
+    // alert(`Até o número: ${ate}`);
+
+    // let numero = obterNumeroAleatorio(de, ate);
+    // alert(numero);
+
+    let sorteados = [];
+    for (let i = 0; i < quantidade; i++) {
+        let numero = obterNumeroAleatorio(de, ate);
+
+        // Verifica se o número já foi sorteado
+        while (sorteados.includes(numero)) {
+            numero = obterNumeroAleatorio(de, ate);
+        }
+
+        sorteados.push(numero);
+    }
+
+    // alert(sorteados);
+
+    let resultado = document.getElementById('resultado');
+    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados}</label>`;
+    alterarStatusDoBotao();
+}
+
+function obterNumeroAleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function alterarStatusDoBotao() {
+    let botaoReiniciar = document.getElementById('btn-reiniciar');
+    if(botaoReiniciar.classList.contains('container__botao-desabilitado')) {
+        botaoReiniciar.classList.remove('container__botao-desabilitado');
+        botaoReiniciar.classList.add('container__botao');
+    } else {
+        botaoReiniciar.classList.remove('container__botao');
+        botaoReiniciar.classList.add('container__botao-desabilitado');
+    }
+}
+
+function reiniciar() {
+    document.getElementById('quantidade').value = '';
+    document.getElementById('de').value = '';
+    document.getElementById('ate').value = '';
+    document.getElementById('resultado').innerHTML = '<label class="texto__paragrafo">Números sorteados: nenhum até agora</label>';
+    
+    alterarStatusDoBotao();
+}
