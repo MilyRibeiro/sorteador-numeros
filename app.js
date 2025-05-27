003626ef1abbee473e825d22eb856e9c39ff314c
Mylena Ribeiro <ribeiro.mylena1@gmail.com>
2025-05-27 13:23:22 -0300

    Proteção na entrada de valores

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ function sortear() {
     let de = parseInt(document.getElementById('de').value);
     let ate = parseInt(document.getElementById('ate').value);
 
+    // Proteção na entrada de valores:
+    if(de >= ate) {
+        alert('O valor "Do número" deve ser menor que o valor do campo "Até o número". Verifique os valores e tente novamente.');
+        return;
+    }
+
     // alert(`Quantidade: ${quantidade}`);
     // alert(`Do número: ${de}`);
     // alert(`Até o número: ${ate}`);
@@ -49,6 +55,6 @@ function reiniciar() {
     document.getElementById('de').value = '';
     document.getElementById('ate').value = '';
     document.getElementById('resultado').innerHTML = '<label class="texto__paragrafo">Números sorteados: nenhum até agora</label>';
-    
+
     alterarStatusDoBotao();
 }
